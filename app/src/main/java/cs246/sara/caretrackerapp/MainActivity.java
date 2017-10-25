5c5286fc8fb99749f4702c2eca0e586512951932
scotttolman <scotttolman@gmail.com>
2017-10-25 09:32:32 -0500

    made another comment

--- app/src/main/java/cs246/sara/caretrackerapp/MainActivity.java
+++ app/src/main/java/cs246/sara/caretrackerapp/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /*Scott Tolman was here.*/
+        /*Scott Tolman was here.*/ 
+        /*Here is another comment by Scott*/
     }
 }
