dac608e97f901d69a7faa5e7c1bcf5cae43e4e8f
scotttolman <tol16014@byui.edu>
2017-10-25 09:41:32 -0500

    Scott added 3rd comment

--- app/src/main/java/cs246/sara/caretrackerapp/MainActivity.java
+++ app/src/main/java/cs246/sara/caretrackerapp/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         /*Scott Tolman was here.*/ 
         /*Here is another comment by Scott*/
+        /*Here is a third comment by Scott*/
     }
 }
