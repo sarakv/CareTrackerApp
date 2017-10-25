921ce0f9c259fca3d7be4893ec59931547c737d5
Sara Olivero <vog13003@byui.edu>
2017-10-25 17:24:46 -0500

    sara's test commit

--- app/src/main/java/cs246/sara/caretrackerapp/MainActivity.java
+++ app/src/main/java/cs246/sara/caretrackerapp/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         /*Here is another comment by Scott*/
         /*Here is a third comment by Scott*/
         // Pedro Lago was here.
+        //Sara was here
     }
 }
