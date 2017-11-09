6fa7efd725b7328497ae6f4561380f058f1c7a38
Sara Olivero <vog13003@byui.edu>
2017-11-09 11:29:25 -0600

    shared pref button test

--- app/src/main/java/cs246/sara/caretrackerapp/MainActivity.java
+++ app/src/main/java/cs246/sara/caretrackerapp/MainActivity.java
@@ -1,21 +1,71 @@
 package cs246.sara.caretrackerapp;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import com.google.gson.Gson;
 
 public class MainActivity extends AppCompatActivity {
+    public class ButtonInfo {
+        public String text;
+        //public Drawable background;
+        public String description;
+        public ViewGroup.LayoutParams params;
+
+        public ButtonInfo() {
+            //intentionally empty
+        }
+
+        public ButtonInfo(Button button) {
+            text = button.getText().toString();
+            //background = button.getBackground().getCurrent();
+            description = button.getTag().toString();
+            params = button.getLayoutParams();
+        }
+
+        public Button getButton(Context context) {
+            Button myButt = new Button(context);
+            myButt.setText(text);
+            myButt.setTag(description);
+            //myButt.setBackground(background);
+            myButt.setLayoutParams(params);
+            myButt.setId(View.generateViewId());
+            return myButt;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /*Scott Tolman was here.*/ 
-        /*Here is another comment by Scott*/
-        /*Here is a third comment by Scott*/
-        // Pedro Lago was here.
-        //Sara was here
-        //nice...
-        //sara!!! again
-        // CHAAAAAAAAAAAAAAAAAAANGE
-    }
+
+        Button testButton = (Button) findViewById(R.id.button);
+
+        Gson gson = new Gson();
+        String btnJson = gson.toJson(new ButtonInfo(testButton));
+
+
+        SharedPreferences.Editor editor = getSharedPreferences("myPref", MODE_PRIVATE).edit();
+        editor.putString("button0", btnJson);
+        editor.apply();
+
+
+        SharedPreferences prefs = getSharedPreferences("myPref", MODE_PRIVATE);
+        btnJson = prefs.getString("button0", "testBtn2");
+
+        Button myButt = gson.fromJson(btnJson, ButtonInfo.class).getButton(this);
+
+        testButton.setText("changed");
+
+        LinearLayout layout = (LinearLayout) findViewById(R.id.testLayout);
+        layout.addView(myButt);
+     }
 }
