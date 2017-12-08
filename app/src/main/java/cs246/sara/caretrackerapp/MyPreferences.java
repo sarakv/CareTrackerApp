dba5efcb023cbec14210e43fad642a09c5a4f523
Sara Olivero <vog13003@byui.edu>
2017-12-07 23:50:44 -0600

    Implemented method to remove key/value pairs

--- app/src/main/java/cs246/sara/caretrackerapp/MyPreferences.java
+++ app/src/main/java/cs246/sara/caretrackerapp/MyPreferences.java
@@ -32,6 +32,19 @@ public class MyPreferences {
     }
 
     /**
+     * Removes the key/value pair from the preferences file
+     *
+     * @param context the application context, required by Android to get the preferences file
+     * @param key    the name of the key to remove
+     */
+    public static void remove(Context context, String key) {
+        SharedPreferences prefs = context.getSharedPreferences(context.getString(R.string.app_name), Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.remove(key);
+        editor.apply();
+    }
+
+    /**
      * Determines the value of an integer parameter
      *
      * @param context the application context, required by Android to get the preferences file
