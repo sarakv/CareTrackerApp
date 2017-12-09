5ef636ac5df8d52bd9b98bd9dc50596b3a7ac1bd
scotttolman <scotttolman@gmail.com>
2017-12-09 13:26:23 -0600

    Added a few unit tests. removed "total" field from expense report

--- app/src/test/java/cs246/sara/caretrackerapp/ExampleUnitTest.java
+++ app/src/test/java/cs246/sara/caretrackerapp/ExampleUnitTest.java
@@ -17,9 +17,35 @@ public class ExampleUnitTest {
     }
 
     @Test
-    public void buttonSettersWork() {
+    public void buttonLabelSetterAndGetterWork() {
         ButtonInfo ButtIn = new ButtonInfo();
         ButtIn.setLabel("my but-ton brings all the boys to the yard");
-        assert(ButtIn.getLabel() == "my but-ton brings all the boys to the yard");
+        assert( ButtIn.getLabel() == "my but-ton brings all the boys to the yard");
+    }
+
+    @Test
+    public void buttonIdSetterAndGetterWork() {
+        ButtonInfo ButtIn = new ButtonInfo();
+        ButtIn.setId(42);
+        assert( ButtIn.getId() == 42);
+    }
+
+    @Test
+    public void buttonColorSetterAndGetterWork() {
+        ButtonInfo ButtIn = new ButtonInfo();
+        ButtIn.setColor(246246);
+        assert( ButtIn.getColor() == 246246);
+    }
+
+    @Test
+    public void sheetDataConstructorSetsGetValuesRetrieves() {
+        SheetData test = new SheetData("Tom Bombadil", "18:00:59",
+                "Don't label me", "This is a test",
+                "Let's get ice cream",
+                "I'm lactose intolarant fool!", "image.png");
+        String[] testResults = test.getValues();
+        for (int i = 0; i < testResults.length; ++i){
+            System.out.println(testResults[i]);
+        }
     }
 }
